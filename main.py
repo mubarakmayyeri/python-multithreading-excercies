0f7b2279eea9652f82b49173b9771411a52fc69e
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-03-15 22:42:33 +0530

    implemented daemon threading

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ import time
 
 done = False
 
-# Target function
+# Target functions
 def worker():
-    global done
     counter = 0
     while not done:
         time.sleep(1)
         counter += 1
         print(counter)
-        if counter == 10:
-            done = True
-        
-        
-        
+
+            
+def display():
+    counter = 0
+    while True:
+        time.sleep(1)
+        counter += 2
+        print(counter)
+      
 # Passing the target function to thread
-threading.Thread(target=worker).start()
+# threading.Thread(target=worker).start()
+
+# Daemon threading
+threading.Thread(target=display, daemon=True).start()
 
 # Getting an input from User
 input("Press 'Enter' to quit the process...\n")
